d4c8574136d659d75a9ef2785cb8256b6fa7efec
phirmware <chibuzor.ojukwu@gmail.com>
2019-06-15 17:15:26 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video 
+    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.79,    // confidence threshold for predictions.
+}
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+// select everything im HTML
+const video = document.querySelector("#video");
+const audio = document.querySelector('#audio');
+const canvas = document.querySelector("#canvas");
+const context = canvas.getContext("2d");
+let model;
+
+
+handTrack.startVideo(video)
+    .then(status => {
+        if (status) {
+            navigator.getUserMedia({ video: {} }, stream => {
+                video.srcObject = stream;
+                setInterval(runDetection, 1000);
+            },
+                err => console.log(err)
+            );
+        }
+    });
+
+var isPlaying = false;
+function togglePlay(){
+    if(isPlaying){
+        audio.pause();
+    } else{
+        audio.play();
+    }
+}
+
+audio.onplaying = function(){
+    isPlaying = true;
+};
+
+audio.onpause = function(){
+    isPlaying = false;
+}
+
+function runDetection() {
+    model.detect(video)
+        .then(predictions => {
+            console.log(predictions);
+            // model.renderPredictions(predictions,canvas,context,video);
+            if(predictions.length > 0){
+                togglePlay();
+            }
+        });
+}
+
+handTrack.load(modelParams)
+    .then(lmodel => {
+        model = lmodel;
+    });
